c6f32e2391227c41e26fedfb5b79efd3202abd16
iiDk-the-actual <54154105+iiDk-the-actual@users.noreply.github.com>
2025-11-22 00:48:26 -0500

    Update Console

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -7,6 +7,6 @@
     {
         public const string GUID = "com.goldentrophy.kingofnetflix.toomanyplayers";
         public const string Name = "TooManyPlayers";
-        public const string Version = "1.1.0";
+        public const string Version = "1.1.1";
     }
 }
